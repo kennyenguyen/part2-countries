fcc5c8faf790ca60d63ad2158b8e212630edfd66
Kenny Nguyen <96954806+kenny-e-nguyen@users.noreply.github.com>
2022-05-17 16:54:42 -0400

    Update search filter

--- src/App.js
+++ src/App.js
@@ -3,10 +3,37 @@ import axios from 'axios'
 
 const App = () => {
     const [countries, setCountries] = useState([])
+    const [filter, setFilter] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('https://restcountries.com/v3.1/all')
+            .then(response => {
+                setCountries(response.data)
+            })
+    }, [])
+
+    const handleFilterChange = (event) => {
+        console.log(event.target.value)
+        setFilter(event.target.value)
+    }
+
+    const countriesToShow = filter
+        ? countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
+        : []
+    
+    const countriesToMap = (countriesToShow.length > 10)
+        ? [{name: {common: 'Too many matches, specify another filter'}}]
+        : countriesToShow
 
     return (
         <>
-            Hello World
+            <div>find countries <input value={filter} onChange={handleFilterChange} /></div>
+            <div>
+                {countriesToMap.map(country =>
+                    <p key={countriesToMap.indexOf(country)}>{country.name.common}</p>    
+                )}
+            </div>
         </>
     )
 }
